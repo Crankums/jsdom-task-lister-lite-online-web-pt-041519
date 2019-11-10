840a04e1746879c1db7c4e91763fd498a86f2897
Craig Donavin <crdonavin@gmail.com>
2019-11-10 22:20:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.querySelector.('#create-task-form')
+  document.querySelector.('#create-task-form').addEventListener("submit")
 });
